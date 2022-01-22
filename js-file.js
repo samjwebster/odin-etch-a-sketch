d2970c2110d4ae1fc210a61dd2b0b9b095ca01ab
samjwebster <samjwebster01@gmail.com>
2022-01-21 21:12:02 -0500

    generate grid, working on ability to interact with blocks

--- js-file.js
+++ js-file.js
@@ -1,2 +1,32 @@
 /* js file for Samuel Webster's etch a sketch project */
 
+const grid = document.getElementById("grid");
+
+var size = 16;
+var setting = 0;
+
+function generateGrid(size) {
+    let i, j;
+
+    for(i = 1; i <= size; i++) {
+        for(j = 1; j <= size; j++) {
+            let newBlock = document.createElement("div");
+            let id = 'block_'+i.toString()+'_'+j.toString();
+            
+            newBlock.toggleAttribute('id', id);
+            newBlock.classList.add("block");
+            newBlock.style.gridRow = i;
+            newBlock.style.gridColumn = j;
+            
+            newBlock.onmouseenter = enterBlock(id);
+
+            grid.appendChild(newBlock);
+        } // end of j
+    } // end of i
+}
+
+function enterBlock(id) {
+    console.log(document.getElementById(id));
+}
+
+generateGrid(size);
