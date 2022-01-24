5078b25b15382dfdc64909bd5c3b6ff30a7d5406
samjwebster <samjwebster01@gmail.com>
2022-01-24 18:55:25 -0500

    beautifying and more functionality

--- js-file.js
+++ js-file.js
@@ -5,9 +5,21 @@ const clear_btn = document.getElementById("clear");
 
 const size_field = document.getElementById("size");
 
+const click_field = document.getElementById("click");
+const rainbow_field = document.getElementById("rainbow");
+const gridlines_field = document.getElementById("gridlines");
+
+
+
+
+
 var setting = 0;
 
-var boxes = [];
+var color = 'rgb(0, 0, 0)';
+const white = 'rgb(255, 255, 255)';
+
+
+
 
 function generateGrid(size) {
 
@@ -26,6 +38,9 @@ function generateGrid(size) {
 
     for(i = 1; i <= size; i++) {
         for(j = 1; j <= size; j++) {
+
+            // creating each individual box
+
             let newBlock = document.createElement("div");
 
             let id = 'block_'+i.toString()+'_'+j.toString();
@@ -36,30 +51,136 @@ function generateGrid(size) {
             newBlock.style.gridRow = i;
             newBlock.style.gridColumn = j;
 
-            boxes.push(newBlock);
-
             grid.appendChild(newBlock);
         } // end of j
     } // end of i
 
     document.querySelectorAll('.block').forEach( item => {
+        /* This works dont delete
+
+        switch(click_field.checked) {
+            case (true): {
+                item.addEventListener('mousedown', event => {
+                    if(item.style.backgroundColor != color) {
+                        item.style.backgroundColor = color;
+                    } else if (item.style.backgroundColor == color) {
+                        item.style.backgroundColor = white;
+                    }
+        
+                    if(item.classList.contains('gridlines')) {
+                        item.classList.remove('gridlines');
+                    }
+                }, false);
+                break;
+            }
+            case (false): {
+                
+                item.addEventListener('mouseenter', event => {
+
+                    item.style.backgroundColor = color;
+        
+                    if(item.classList.contains('gridlines')) {
+                        item.classList.remove('gridlines');
+                    }
+                }, false);
+            }
+        }*/
+
+
+        // HOVERING
         item.addEventListener('mouseenter', event => {
-            item.style.backgroundColor = "black";
+            if(click_field.checked == false) {
+                item.style.backgroundColor = color;
+
+                if(item.classList.contains('gridlines')) {
+                    item.classList.remove('gridlines');
+                }
+            }
         }, false);
+
+        // CLICKING
+        item.addEventListener('mousedown', event => {
+            if(click_field.checked == true) {
+
+                //white to black
+                if(item.style.backgroundColor == white) {
+                    item.style.backgroundColor = color;
+
+                    if(gridlines_field.checked == true) {
+                        console.log('fuck gridlines!');
+                        item.classList.remove('gridlines');
+                    }
+                //black to white
+                } else if (item.style.backgroundColor == color) {
+                    item.style.backgroundColor = white;
+
+                    if(gridlines_field.checked == true) {
+                        console.log('adding gridlines!');
+                        item.classList.add('gridlines');
+                    }
+                }
+
+                if(item.classList.contains('gridlines')) {
+                    item.classList.remove('gridlines');
+                }
+            }
+        }, false);
+    
     });
 }
 
 function clearFunc() {
+    /*
     document.querySelectorAll('.block').forEach( item => {
-        item.style.backgroundColor = "white";
+        item.classList.remove('active');
     });
+    */
+
+
+    document.querySelectorAll('.block').forEach( item => {
+        item.style.backgroundColor = white;
+    });
+
+
+    if(gridlines_field.checked == true) {
+        document.querySelectorAll('.block').forEach( item => {
+            item.classList.add('gridlines');
+        });
+    }
 }
 
 function init() {
     clear_btn.onclick = clearFunc;
 
+
     size_field.addEventListener('input', function () {
         generateGrid(size_field.value)
+    });
+
+    /*
+    click_field.addEventListener('input', function () {
+        generateGrid(size_field.value);
+    });
+    */
+
+
+    gridlines_field.addEventListener('input', function () {
+        document.querySelectorAll('.block').forEach( item => {
+            switch(gridlines_field.checked) {
+                case (true): {
+                    if(item.style.backgroundColor != color) {
+                        item.classList.add('gridlines');
+                    }
+                    break;
+                }
+                case (false): {
+                    item.classList.remove('gridlines');
+                    break;
+                }
+            }
+
+
+        });
     });
 
     generateGrid(16);
